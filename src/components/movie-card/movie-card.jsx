9a7e9214080192cf6c1981c40061d64340d3c50b
Tova Osborne <tovaoz@gmail.com>
2023-09-01 11:13:52 -0700

    search bar

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,16 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const MovieCard = ({ movieData }) => {
+  const [query, setQuery] = useState("");
+  console.log(movieData)
   return (
+    // <input type="text" placeholder="Search..." className="search" onChange={e => setQuery(e.target.value)}/>
+    {movieData.filter((movie) => 
+      movieData.title.toLowerCase().includes(query)).map((movie) => (
     <Card className="h-100 text-center mb-5" border="dark">
       <Card.Img variant="top" src={movieData.image} alt="Movie Poster" />
       <Card.Body>
@@ -13,6 +20,7 @@ export const MovieCard = ({ movieData }) => {
         </Link>
       </Card.Body>
     </Card>
+    ))}
   );
 };
 
